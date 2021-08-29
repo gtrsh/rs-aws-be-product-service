edfb695d7f303255d6eb3cc07554399721cb7b7b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-08-29 23:27:59 +0300

    fix: add headers for fixing cors

--- src/getProductsList.js
+++ src/getProductsList.js
@@ -3,6 +3,12 @@ import { data } from './mockData';
 const getProductsListHandler = async (event, ctx, cb) => {
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(data)
     };
 };
